eeea532e3b815ef60f81aef6d60f422c834b6806
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-10-06 00:23:48 +0530

    Added Generics and function types

--- Basics.ts
+++ Basics.ts
@@ -3,6 +3,11 @@
 // Reference - array, object
 // Function types, parameters
 
+type Person = {
+    name: string;
+    age: number;
+}
+
 let age: number;
 age = 23;
 
@@ -17,10 +22,7 @@ isInstructor = false;
 let a : string[];
 a = ['12', '16'];
 
-let person: {
-    name: string;
-    age: number;
-};
+let person: Person;
 
 person = {
     name: 'Maz',
@@ -31,11 +33,29 @@ person = {
 //     isEmployee: true,
 // }
 
-let people: {
-    name: string;
-    age: number;
-}[];
+let people: Person[];
 
 // Type inference - It's a good practice
-// let course = 'React bootcamp';
-// course = 12;
+let course: string | number = 'React bootcamp';
+course = 12;
+
+// Function types
+
+function add(a:number, b: number) {
+    return a+b;
+}
+
+function logMessage(value: any) {
+    console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    return [...array, value];
+}
+
+const updatedArray = insertAtBeginning([1, 2, 3], 4);
+// updatedArray[0].split('');
+
+const updatedStringArr = insertAtBeginning(['1', '3', '4'], 'asd');
