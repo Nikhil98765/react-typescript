961173a6b04859b9428ac8fd067fdb94bbfddcba
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-10-05 23:24:35 +0530

    Added basic types

---
+++ Basics.ts
@@ -0,0 +1,37 @@
+
+// primitive - number, string and boolean
+// Reference - array, object
+// Function types, parameters
+
+let age: number;
+age = 23;
+
+let userName: string;
+userName = 'Nikhil';
+
+let isInstructor: boolean;
+isInstructor = false;
+
+// arrays and objects
+
+let a : string[];
+a = ['12', '16'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Maz',
+    age: 23
+};
+
+// person = {
+//     isEmployee: true,
+// }
+
+let people: {
+    name: string;
+    age: number;
+}[];
